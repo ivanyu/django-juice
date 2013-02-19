f21f12cdec457967aadcb5d03650237b4cf88aee
Ivan Yurchenko <ivanhoe12@gmail.com>
2013-02-19 23:46:02 +0400

    Some setup changes

--- juice/__init__.py
+++ juice/__init__.py
@@ -2,3 +2,5 @@
 django-juice is a collection of small reusable Django utilities in form of
 Django app.
 """
+
+VERSION = "0.1b"
--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from setuptools import setup
 import os
 
+from juice import VERSION
+
 
 # Allow setup.py to be run from any path.
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 NAME = "django-juice"
-VERSION = "0.1b"
 
 DESCRIPTION = "A collection of small reusable Django utilities in form of Django app."
 LONG_DESCRIPTION = DESCRIPTION
@@ -18,6 +19,8 @@ AUTHOR_EMAIL = "ivan0yurchenko@gmail.com"
 
 MAINTAINER = AUTHOR
 MAINTAINER_EMAIL = AUTHOR_EMAIL
+
+URL = "https://github.com/ivanyu/django-juice/"
 
 setup(
     name=NAME,
@@ -29,13 +32,14 @@ setup(
     author_email=AUTHOR_EMAIL,
     maintainer=MAINTAINER,
     maintainer_email=MAINTAINER_EMAIL,
-    url="https://github.com/ivanyu/django-juice/",
+    url=URL,
     license="MIT License",
     packages=["juice"],
     zip_safe=False,
     install_requires=[],
     include_package_data=True,
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python",
         "Framework :: Django",
         "Environment :: Web Environment",
