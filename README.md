fb673e98b7cac135f2836b82731225503d7067df
Ivan Yurchenko <ivanhoe12@gmail.com>
2013-02-19 11:49:18 +0400

    TrimCharField and TrimCharFieldsModelFormMetaclass added

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Mixins:
 * `LoginRequiredMixin` - a mixin which makes class-based view available only for authenticated users.
 * `UrlKwargsMixing` - a mixin which extracts required kwargs from the url and makes them available inside a view as attributes.
 
+Form utils:
+* `TrimCharField` - a char field which truncates it's value to `max_length` before validation.
+* `TrimCharFieldsModelFormMetaclass` - a metaclass for ModelForm which replaces `CharField` with `TrimCharField` for fields listed in `Meta.trim_fields`.
+
 License
 =======
 The application is distributed under MIT license (see LICENSE.txt).
--- juice/forms.py
+++ juice/forms.py
@@ -6,12 +6,16 @@ A collection of form utils.
 validation.
 """
 from __future__ import unicode_literals
+
 from django.forms import CharField
+from django.forms import ModelForm
+from django.forms.models import ModelFormMetaclass
+from django.core.exceptions import ImproperlyConfigured
 
 
 class TrimCharField(CharField):
     """
-    A char field which truncates it's value to max_length before validation.
+    A char field which truncates it's value to `max_length` before validation.
     """
 
     def to_python(self, value):
@@ -20,3 +24,52 @@ class TrimCharField(CharField):
         if self.max_length is not None:
             py_value = py_value[:self.max_length]
         return py_value
+
+
+class TrimCharFieldsModelFormMetaclass(ModelFormMetaclass):
+    """
+    A metaclass for ModelForm which replaces `CharField` with `TrimCharField`
+    for fields listed in `Meta.trim_fields`.
+    """
+
+    def __new__(cls, name, bases, attrs):
+        new_class = super(TrimCharFieldsModelFormMetaclass, cls).__new__(
+            cls, name, bases, attrs)
+
+        # Raise an exception in case the metaclass used with not
+        if not issubclass(new_class, ModelForm):
+            raise TypeError("Class '{}' must be derived from ModelForm".
+                format(name))
+
+        # Ignore NewBase class inserted by six. Also we're not interested in
+        # classes without Meta.
+        if name == 'NewBase' or 'Meta' not in attrs:
+            return new_class
+
+        trim_fields = attrs['Meta'].__dict__.get('trim_fields', [])
+
+        for f in trim_fields:
+            if f not in new_class.base_fields:
+                raise ImproperlyConfigured(
+                    "Trim field '{}' doesn't exist in base_fields.".format(f))
+
+            field = new_class.base_fields[f]
+            if not isinstance(field, CharField):
+                raise TypeError(
+                    "Trim field '{}' must be CharField, {} given.".format(
+                        f, field.__class__.__name__))
+
+            trimField = TrimCharField(
+                required=field.required,
+                label=field.label,
+                initial=field.initial,
+                widget=field.widget,
+                error_messages=field.error_messages,
+                validators=field.validators,
+                localize=field.localize,
+                max_length=field.max_length,
+                min_length=field.min_length
+                )
+            new_class.base_fields[f] = trimField
+
+        return new_class
