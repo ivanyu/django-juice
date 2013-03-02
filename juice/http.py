fcf321a639b7ee86b110adcd958764e27aa85f0b
Ivan Yurchenko <ivanhoe12@gmail.com>
2013-03-02 14:21:52 +0400

    JsonResponse simplified

--- juice/http.py
+++ juice/http.py
@@ -4,6 +4,6 @@ from django.utils import simplejson
 
 class JsonResponse(HttpResponse):
     def __init__(self, dictionary):
-        content = simplejson.dumps(dictionary)
         super(JsonResponse, self).__init__(
-            content, content_type='application/json')
+            simplejson.dumps(dictionary),
+            content_type='application/json')
