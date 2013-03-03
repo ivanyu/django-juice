d90df3b70fcafff9915b33273771a044798572da
Ivan Yurchenko <ivanhoe12@gmail.com>
2013-03-04 00:01:58 +0400

    Request parameters extraction generalized. GET method added

--- juice/views/generic/ajax_action.py
+++ juice/views/generic/ajax_action.py
@@ -11,45 +11,48 @@ class ParamExtractionError(Exception):
     pass
 
 
-class AjaxActionView(View):
-    """
-
-    """
-
-    http_method_names = ['post']
-    post_parameters = []
-
-    # TODO custom hanlers
-
+class RequestParamsExtractingMixin(object):
     def __init__(self, *args, **kwargs):
-        super(AjaxActionView, self).__init__(*args, **kwargs)
+        super(RequestParamsExtractingMixin, self).__init__(*args, **kwargs)
+
+        if not 'method' in kwargs:
+            raise ImproperlyConfigured(
+                "Keyword argument 'method' is required "
+                "with possible values 'post' or 'get'.")
+        self.method = kwargs['method']
+        if self.method not in ['post', 'get']:
+            raise ImproperlyConfigured(
+                "Allowed values for 'method' are 'post' and 'get'.")
 
         def get_handler(param_name, param_type):
             attr_name = '_'+param_name
 
             def str_handler(self, request):
-                if param_name not in request.POST:
+                params = getattr(request, self.method.upper())
+                if param_name not in params:
                     raise ParamExtractionError
                 setattr(self, attr_name, request.POST[param_name])
 
             def int_handler(self, request):
+                params = getattr(request, self.method.upper())
                 try:
                     setattr(self,
                             attr_name,
-                            int(request.POST.get(param_name, None)))
+                            int(params.get(param_name, None)))
                 except (TypeError, ValueError):
                     raise ParamExtractionError
 
             def bool_handler(self, request):
-                value = request.POST.get(param_name, None)
+                params = getattr(request, self.method.upper())
+                value = params.get(param_name, None)
                 if value is None or value.lower() not in ['true', 'false']:
                     raise ParamExtractionError
                 setattr(self, attr_name, value.lower() == 'true')
 
             return locals()[param_type + '_handler']
 
-        norm_post_parameters = []
-        for x in self.post_parameters:
+        norm_parameters = []
+        for x in self._get_param_dict():
 
             if isinstance(x, six.string_types):
                 param_name, param_type = x, 'str'
@@ -60,23 +63,57 @@ class AjaxActionView(View):
                 handler = MethodType(
                     get_handler(param_name, param_type), self)
             elif param_type == 'custom':
-                print '2'
                 handler = getattr(self, "_handle_"+param_name)
             else:
                 msg = "Unknown param type '{0}'".format(param_type)
                 raise ImproperlyConfigured(msg)
 
-            norm_post_parameters.append((x, handler))
-        self.post_parameters = norm_post_parameters
+            norm_parameters.append((x, handler))
+        setattr(self, self._get_param_dict_name(), norm_parameters)
+
+    def _get_param_dict_name(self):
+        return self.method + '_parameters'
+
+    def _get_param_dict(self):
+        return getattr(self, self._get_param_dict_name())
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method.lower() == self.method:
+            try:
+                for x, handler in self._get_param_dict():
+                    handler(request)
+            except ParamExtractionError:
+                return HttpResponseBadRequest()
+        return super(RequestParamsExtractingMixin, self).dispatch(
+            request, *args, **kwargs)
+
+
+class PostParamsExtractingMixin(RequestParamsExtractingMixin):
+    post_parameters = []
+
+    def __init__(self, *args, **kwargs):
+        kwargs['method'] = 'post'
+        super(PostParamsExtractingMixin, self).__init__(*args, **kwargs)
+
+
+class GetParamsExtractingMixin(RequestParamsExtractingMixin):
+    get_parameters = []
+
+    def __init__(self, *args, **kwargs):
+        kwargs['method'] = 'get'
+        super(GetParamsExtractingMixin, self).__init__(*args, **kwargs)
+
+
+class AjaxActionView(PostParamsExtractingMixin,
+                     View):
+    """
+
+    """
+
+    http_method_names = ['post']
 
     def post(self, request, *args, **kwargs):
         if not request.is_ajax():
             return HttpResponseBadRequest("Not Ajax request.")
-
-        try:
-            for x, handler in self.post_parameters:
-                handler(request)
-        except ParamExtractionError:
-            return HttpResponseBadRequest()
 
         return self.action(request)
